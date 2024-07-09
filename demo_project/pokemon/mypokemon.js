a7588ec92a1bf145ec1e28c9f579a1b970c08aec
koyuki0421 <koyuki20000421@gmail.com>
2024-07-09 18:15:30 +0800

    demoproject

---
+++ demo_project/pokemon/mypokemon.js
@@ -0,0 +1,22 @@
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+const container = document.querySelector('#container');
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+
+for (let i = 1; i <= 150; i++) {
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon');
+    const label = document.createElement('span');
+    label.innerText = `#${i}`;
+    const newImg = document.createElement('img');
+    newImg.src = `${baseURL}${i}.png`
+
+
+    pokemon.appendChild(newImg);
+    // 把div放入圖片
+    pokemon.appendChild(label);
+    // 把div放入#1等文字
+    container.appendChild(pokemon)
+    // 把div放進container裡面
+}
