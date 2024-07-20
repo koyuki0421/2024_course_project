058150da8a3d15a0881359c5928e4ec8228c3359
koyuki0421 <koyuki20000421@gmail.com>
2024-07-20 23:31:47 +0800

    20240720done

--- express/For EJS/index.js
+++ express/For EJS/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
 
+// 這邊使用"靜態資產or靜態檔"且根據public的目錄(通常位置放在根目錄那邊)
+// 老師習慣public目錄裡放css檔案根js檔案
+// 連接方法看subrebbit.ejs的link
+// 寫法使用絕對路徑的方式path.join(__dirname, 'public')
+app.use(express.static(path.join(__dirname, 'public')))
+
+// 我的路由:http://localhost:3000
 app.listen(3000, () => {
     console.log("Listening on Port 3000")
 })
@@ -18,4 +26,27 @@ app.set('views', path.join(__dirname, '/views'))
 app.get('/', (req, res) => {
     res.render('home')
 })
-// 不用/views/home，因為views是預設目錄，也不用.ejs，因為第10行已經設定.ejs檔了
+// 不用/views/home，因為views是預設目錄，也不用.ejs，因為第18行已經設定.ejs檔了
+
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random', { rand: num })
+})
+// {rand :num}的意思是傳遞第二個變數，是一個object，前面是keyname後面是value
+// 也可以key value同名{ num }這樣即可
+
+app.get('/r/:subrebbit', (req, res) => {
+    const { subrebbit } = req.params;
+    const data = redditData[subrebbit];
+    if (data) {
+        res.render('subrebbit', { ...data });
+    } else {
+        res.render('notfound', { subrebbit })
+    }
+})
+
+app.get('/cats', (req, res) => {
+    const cats = ['a', 'b', 'c', 'd', 'e'];
+    // 假設有一個array
+    res.render('cats', { cats })
+})
